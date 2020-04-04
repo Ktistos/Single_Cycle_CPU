1b57347774bc589258674af21694b5565711e9f1
Konstantinos Ktistakis <ktistkostas@gmail.com>
2020-04-05 00:00:00 +0300

    Update MUX2To1_5_tb.vhd

--- simulations/MUX2To1_5_tb.vhd
+++ simulations/MUX2To1_5_tb.vhd
@@ -1,37 +1,7 @@
---------------------------------------------------------------------------------
--- Company: 
--- Engineer:
---
--- Create Date:   19:04:30 03/18/2020
--- Design Name:   
--- Module Name:   /home/dbanelas/Documents/14.7/ISE_DS/CPU_wannabe/MUX2To1_5_tb.vhd
--- Project Name:  CPU_wannabe
--- Target Device:  
--- Tool versions:  
--- Description:   
--- 
--- VHDL Test Bench Created by ISE for module: MUX2To1_5
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
---------------------------------------------------------------------------------
+
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
- 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
- 
+
 ENTITY MUX2To1_5_tb IS
 END MUX2To1_5_tb;
  
@@ -78,11 +48,12 @@ BEGIN
    stim_proc: process
    begin		
       -- hold reset state for 100 ns.
+     --checking if A will show up in the output
       wait for 100 ns;	
        A<="11111";
        B<="00001";
        Sel<='0';
-       
+       --checking if B will show up in the output
        wait for 100ns;
        Sel<='1';
 
