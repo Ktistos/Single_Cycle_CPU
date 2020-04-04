0f23371c2898cac6fb35277285f941412c2e7b34
Konstantinos Ktistakis <ktistkostas@gmail.com>
2020-04-04 23:27:23 +0300

    Update DECSTAGE_tb.vhd

--- simulations/DECSTAGE_tb.vhd
+++ simulations/DECSTAGE_tb.vhd
@@ -1,38 +1,11 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/18/2020 08:27:59 PM
--- Design Name: 
--- Module Name: DECSTAGE_tb - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
+--The purpose of this testbench is to check whether the submodules of DECSTAGE are properly connected 
+--and that registers can be read or written using an instruction
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity DECSTAGE_tb is
---  Port ( );
+
 end DECSTAGE_tb;
 
 architecture Behavioral of DECSTAGE_tb is
@@ -103,9 +76,9 @@ port map(
    
    stim_proc: process
    begin
-   
+   --The following instructions are not actual instructions.They were created to check whether that the register file can be read/written using the appropriate instruction format
    wait for 100ns;
-   --writing in registers 1 the contents of MEM
+   --writing in register 1 the contents of MEM
    Instr<=x"00010000";
    RF_WrEn<='1';
    MEM_out<=x"11111111";
@@ -142,6 +115,3 @@ port map(
 
    
    end process;
-
-
-end Behavioral;
