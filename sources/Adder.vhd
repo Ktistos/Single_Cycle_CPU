14488f8787793d1b01c46cbba64210f047367cfd
Konstantinos Ktistakis <ktistkostas@gmail.com>
2020-04-06 00:18:41 +0300

    Update Adder.vhd

--- sources/Adder.vhd
+++ sources/Adder.vhd
@@ -1,32 +1,7 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/17/2020 06:44:44 PM
--- Design Name: 
--- Module Name: Adder - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity Adder is
   Port ( 
