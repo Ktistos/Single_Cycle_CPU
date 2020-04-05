8b852d21a27f9ff0127e89458ee80e85515640de
Konstantinos Ktistakis <ktistkostas@gmail.com>
2020-04-06 00:21:21 +0300

    Update DATAPATH.vhd

--- sources/DATAPATH.vhd
+++ sources/DATAPATH.vhd
@@ -1,35 +1,6 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 03/29/2020 04:27:07 PM
--- Design Name: 
--- Module Name: DATAPATH - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
 
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
 
 entity DATAPATH is
   Port ( 
