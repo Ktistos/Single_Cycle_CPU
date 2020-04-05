6ca73f314138505b53e8d00b48a3f54d296d6b91
Konstantinos Ktistakis <ktistkostas@gmail.com>
2020-04-06 00:20:07 +0300

    Update ALU_CONTROL.vhd

--- sources/ALU_CONTROL.vhd
+++ sources/ALU_CONTROL.vhd
@@ -4,15 +4,6 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
--- Uncomment the following library declaration if using
--- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
-
--- Uncomment the following library declaration if instantiating
--- any Xilinx leaf cells in this code.
---library UNISIM;
---use UNISIM.VComponents.all;
-
 entity ALU_CONTROL is
   Port ( 
         Func : in std_logic_vector(5 downto 0);
